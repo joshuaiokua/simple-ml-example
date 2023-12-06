4c9f8509f273837a9675c5fb05d34afc0639654f
joshuaiokua <32827030+joshuaiokua@users.noreply.github.com>
2023-12-05 19:30:49 -0800

    feat: complex example

---
+++ lamini_example.py
@@ -0,0 +1,50 @@
+"""
+    Example taken from Lamini.
+
+    Original file is located at
+        https://colab.research.google.com/drive/10VFX_wBEZZV7rdySscKL0DCq5Qlpkzjo
+"""
+
+import os
+import dotenv
+import time
+from llama import QuestionAnswerModel
+
+def download_files_from_google_drive():
+    os.system("""
+    wget -q -O "seed_lamini_docs.jsonl" "https://drive.google.com/uc?export=download&id=1SfGp1tVuLTs0WYDugZcxX-EHrmDtYrYJ"
+    wget -q -O "seed_taylor_swift.jsonl" "https://drive.google.com/uc?export=download&id=119sHYYImcXEbGyvS3wWGpkSEVIFdLy6Z"
+    wget -q -O "seed_bts.csv" "https://drive.google.com/uc?export=download&id=1lblhdhKwoiOjlvfk8tr7Ieo4KpvjRm6n"
+    wget -q -O "seed_open_llm.jsonl" "https://drive.google.com/uc?export=download&id=1S7oPPko-UmOr-bqkZ_PREfGKO2f73ZiK"
+    """)
+
+def print_training_results(results):
+    print("-"*100)
+    print("Training Results")
+    print(results)
+    print("-"*100)
+
+def print_inference(question, finetune_answer, base_answer):
+    print('Running Inference for: '+ question)
+    print("-"*100)
+    print("Finetune model answer: ", finetune_answer)
+    print("-"*100)
+    print("Base model answer: ", base_answer)
+    print("-"*100)
+
+if __name__ == "lamini_example":
+    dotenv.load_dotenv()
+    download_files_from_google_drive()
+
+    # Train the model on the seed data
+    finetune_model = QuestionAnswerModel()
+    finetune_model.load_question_answer_from_jsonlines("seed_lamini_docs.jsonl")
+
+    # Train the model
+    start=time.time()
+    finetune_model.train(enable_peft=True)
+    #print(f"Time taken: {time.time()-start} seconds")
+
+    # Evaluate base and finetuned models to compare performance
+    results = finetune_model.get_eval_results()
+    print_training_results(results)
